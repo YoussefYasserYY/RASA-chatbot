d83761749010ee048d829fa262c3f5702481c327
Youssef Yasser <yousefyasser8292@gmail.com>
2024-05-21 03:55:19 +0300

    - trained new rasa model for egyptian chat - fixed a bug with the audio player of the bot

--- app.py
+++ app.py
@@ -16,6 +16,61 @@ recognizer = sr.Recognizer()
 RASA_URL = "http://localhost:5005"
 
 
+
+
+
+
+
+import os
+import uuid
+from elevenlabs import VoiceSettings
+from elevenlabs.client import ElevenLabs
+
+ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
+client = ElevenLabs(
+    api_key='25ac2695ad6b513ea9ad2b9467a8e30e',
+)
+
+
+
+def text_to_speech_file(text: str) -> str:
+    # Calling the text_to_speech conversion API with detailed parameters
+    response = client.text_to_speech.convert(
+        voice_id="pNInz6obpgDQGcFmaJgB", # Adam pre-made voice
+        optimize_streaming_latency="0",
+        output_format="mp3_22050_32",
+        text=text,
+        model_id="eleven_multilingual_v2", # use the turbo model for low latency, for other languages use the `eleven_multilingual_v2`
+        voice_settings=VoiceSettings(
+            stability=0.0,
+            similarity_boost=1.0,
+            style=0.0,
+            use_speaker_boost=True,
+        ),
+    )
+
+    # uncomment the line below to play the audio back
+    # play(response)
+
+    # Generating a unique file name for the output MP3 file
+    save_file_path = "Rasa_answer/answer.mp3"
+
+    # Writing the audio to a file
+    with open(save_file_path, "wb") as f:
+        for chunk in response:
+            if chunk:
+                f.write(chunk)
+
+    # Return the path of the saved audio file
+    return save_file_path
+
+
+
+
+
+
+
+
 def send_to_rasa(text):
     """
     Send a message to Rasa server and get the response.
@@ -57,8 +112,11 @@ def send_message():
     """
     message = request.form["message"]
     rasa_response = send_to_rasa(message)
-    tts = gTTS(text=rasa_response, lang="ar")
-    tts.save('Rasa_answer/answer.mp3')
+    text_to_speech_file(rasa_response)
+
+    
+    # tts = gTTS(text=rasa_response, lang="ar")
+    # tts.save('Rasa_answer/answer.mp3')
     return jsonify({"audio_url": "/stream_audio", "message": rasa_response})
 
 @app.route("/stream_audio")
